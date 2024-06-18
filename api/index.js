f366122e8fb8db6771a2011a515369c2ac1648bf
DerianDev <derian.nazareno@hotmail.com>
2024-06-17 19:39:40 -0500

    actualizar index.js

--- api/index.js
+++ api/index.js
@@ -5,9 +5,13 @@ const app = express();
 app.use(
   '/',
   createProxyMiddleware({
-    target: 'http://127.0.0.1:8000', // Esto asume que Laravel está corriendo en este puerto
+    target: 'http://127.0.0.1:3306', // Laravel server
     changeOrigin: true,
   })
 );
+
+app.listen(3000, () => {
+  console.log('Proxy server running on port 3000');
+});
 
 module.exports = app;
